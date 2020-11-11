60f7794a8dbd08ab52c4ca5dcb6d89fa3348b924
Peter Chau <peter.chau-lo@peartherapeutics.com>
2020-11-10 22:48:34 -0800

    hooks

--- src/renderer/pages/StartPage.tsx
+++ src/renderer/pages/StartPage.tsx
@@ -11,96 +11,82 @@ type Props = {
 	latestLauncherVersion?: string,
 	os: string,
 }
-type State = {
-	installedClientVersion?: string,
-}
 
-class StartPage extends Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			installedClientVersion: storage.getItem('installedClientVersion'),
-		}
-	}
+const StartPage = ({
+   latestLauncherVersion,
+   latestClientVersion,
+   os
+}: Props) => {
+	const installedClientVersion = storage.getItem('installedClientVersion')
 
-	componentDidMount(): void {
-		this.setState({
-			installedClientVersion: storage.getItem('installedClientVersion'),
-		})
-	}
-
-	render() {
-		const { latestLauncherVersion, latestClientVersion, os } = this.props
-		const { installedClientVersion,  } = this.state
-
-		return(
-			<div>
-				<img
-					src={city}
-					style={{
-						height: '100%',
-						width: '100%',
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						zIndex: 0,
-						objectFit: 'cover'
-					}}
-				/>
+	return (
+		<div>
+			<img
+				src={city}
+				style={{
+					height: '100%',
+					width: '100%',
+					position: 'absolute',
+					top: 0,
+					left: 0,
+					right: 0,
+					bottom: 0,
+					zIndex: 0,
+					objectFit: 'cover'
+				}}
+			/>
+			<div
+				style={{
+					height: '100%',
+					width: '100%',
+					top: 0,
+					bottom: 0,
+					left: 0,
+					right: 0,
+					position: 'absolute',
+					display: 'grid',
+					gridTemplateRows: '90vh 10vh'
+				}}>
 				<div
 					style={{
-						height: '100%',
-						width: '100%',
-						top: 0,
-						bottom: 0,
-						left: 0,
-						right: 0,
-						position: 'absolute',
-						display: 'grid',
-						gridTemplateRows: '90vh 10vh'
-					}}>
+						gridRowStart: 1,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				>
+					<Header />
 					<div
 						style={{
-							gridRowStart: 1,
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-							justifyContent: 'center',
+							marginBottom: '24px',
+							display: 'flex'
 						}}
 					>
-						<Header />
-						<div
-							style={{
-								marginBottom: '24px',
-								display: 'flex'
-							}}
-						>
-							<CTA
-								installedClientVersion={installedClientVersion}
-								latestClientVersion={latestClientVersion}
-								os={os}
-							/>
-						</div>
-						<LauncherVersion
-							latestLauncherVersion={latestLauncherVersion}
-						/>
-					</div>
-					<div
-						style={{
-							gridRowStart: 2,
-							display: 'flex',
-							alignItems: 'flex-end',
-						}}
-					>
-						<Version
+						<CTA
 							installedClientVersion={installedClientVersion}
+							latestClientVersion={latestClientVersion}
+							os={os}
 						/>
 					</div>
+					<LauncherVersion
+						latestLauncherVersion={latestLauncherVersion}
+					/>
 				</div>
-		  </div>
-		);
-  	}
+				<div
+					style={{
+						gridRowStart: 2,
+						display: 'flex',
+						alignItems: 'flex-end',
+					}}
+				>
+					<Version
+						installedClientVersion={installedClientVersion}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
+
 export default StartPage;
